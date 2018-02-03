2df3937c02838136a9438709377122d690a76942
Marek Benc <dusxmt@gmx.com>
2018-02-03 15:47:39 +0100

    Added proper command-line argument handling.

--- src/main.c
+++ src/main.c
@@ -18,42 +18,91 @@
 #include "descriptions.h"
 #include "operations.h"
 
+static gboolean opt_delete    = FALSE;
+static gboolean opt_checksum  = FALSE;
+static gboolean opt_size_only = FALSE;
+
+static GOptionEntry opt_entries[] =
+{
+  { "size-only", 's', 0, G_OPTION_ARG_NONE, &opt_size_only, "Skip files that match in size", NULL },
+  { "checksum",  'c', 0, G_OPTION_ARG_NONE, &opt_checksum,  "Skip files based on checksum, not size and modified time", NULL },
+  { "delete",    'd', 0, G_OPTION_ARG_NONE, &opt_delete,    "Delete extraneous files from destination directories", NULL },
+  { NULL }
+};
+
 int
 main (int argc, char **argv)
 {
+  GOptionContext *opt_context;
   RudgiosyncDirectoryEntry *source;
   RudgiosyncDirectoryEntry *destination;
+
+  const gchar *src_uri;
+  const gchar *dest_uri;
+
   GError *ierror = NULL;
 
-  if (argc != 3)
+
+  g_type_init();
+  opt_context = g_option_context_new ("<source> <destination>");
+  g_option_context_set_summary (opt_context,
+                               "rudgiosync is a simplistic file synchronizing utility, inspired heavily by\n"
+                               "rsync, which uses GIO as its I/O library, and can therefore natively access\n"
+                               "and synchronize gvfs-based filesystems.\n"
+                               "\n"
+                               "The <source> and <destination> arguments must be valid GIO-supported URIs.");
+  g_option_context_add_main_entries (opt_context, opt_entries, NULL);
+  g_option_context_parse (opt_context, &argc, &argv, &ierror);
+  if (ierror != NULL)
     {
-      g_printerr ("%s: Exactly two arguments are requested.\n", argv[0]);
+      g_printerr ("%s: Command line option parsing failed: %s.\n", g_get_prgname (), ierror->message);
+      g_clear_error (&ierror);
       return 1;
     }
-  g_type_init();
+
+  if (argc < 2)
+    {
+      g_printerr ("%s: Command line option parsing failed: %s.\n", g_get_prgname (), "Source location missing");
+      return 1;
+    }
+  src_uri = argv[1];
+
+  if (argc < 3)
+    {
+      g_printerr ("%s: Command line option parsing failed: %s.\n", g_get_prgname (), "Destination location missing");
+      return 1;
+    }
+  dest_uri = argv[2];
+
+  if (argc > 3)
+    {
+      g_printerr ("%s: Command line option parsing failed: Unknown option `%s'.\n", g_get_prgname (), argv[3]);
+      return 1;
+    }
+
 
   g_print ("Examining source directory tree... ");
-  source = rudgiosync_directory_entry_new (argv[1], FALSE, &ierror);
+  source = rudgiosync_directory_entry_new (src_uri, FALSE, &ierror);
   g_print ("done.\n");
   if (ierror != NULL)
     {
-      g_printerr ("%s: Failed to investigate the source: %s.\n", argv[0], ierror->message);
+      g_printerr ("%s: Failed to investigate the source: %s.\n", g_get_prgname (), ierror->message);
       return 1;
     }
 
   g_print ("Examining destination directory tree... ");
-  destination = rudgiosync_directory_entry_new (argv[2], FALSE, &ierror);
+  destination = rudgiosync_directory_entry_new (dest_uri, FALSE, &ierror);
   g_print ("done.\n");
   if (ierror != NULL)
     {
-      g_printerr ("%s: Failed to investigate the destination: %s.\n", argv[0], ierror->message);
+      g_printerr ("%s: Failed to investigate the destination: %s.\n", g_get_prgname (), ierror->message);
       return 1;
     }
 
-  rudgiosync_synchronize (&destination, &source, TRUE, FALSE, FALSE, &ierror);
+  rudgiosync_synchronize (&destination, &source, !opt_size_only, opt_checksum, opt_delete, &ierror);
   if (ierror != NULL)
     {
-      g_print ("Synchronization failed: %s.\n", ierror->message);
+      g_printerr ("%s: Synchronization failed: %s.\n", g_get_prgname (), ierror->message);
       g_clear_error (&ierror);
     }
   /*
